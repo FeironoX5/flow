34ce72ed4fcc7e6342bdae77d18d19930472eebd
Gleb Kiva <feironox5@gmail.com>
2024-12-20 21:29:23 +0300

    readme & routing updated

--- src/routes.ts
+++ src/routes.ts
@@ -30,6 +30,7 @@ export const routes: Routes = [
           { path: '', outlet: 'header', component: DataDisplayHeaderComponent },
         ],
       },
+      { path: '**', redirectTo: '/flow', pathMatch: 'full' },
     ],
   },
 ];
